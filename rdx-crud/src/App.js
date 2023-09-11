781a354a5a2b4446744da2d56cf61e67cf8a565d
abed1373 <abedrivaz123456@gmail.com>
2023-09-11 13:28:26 +0330

    first commit

---
+++ rdx-crud/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import { useSelector,useDispatch } from 'react-redux';
+import { addUser,deleteUser,updateUsername } from './features/Users';
+
+import { useState } from 'react';
+
+function App() {
+  const dispatch=useDispatch()
+const userList=useSelector(state=>state.users.value)
+
+const [name,setName]=useState('');
+const [username,setUsername]=useState('');
+const [newUsername,setNewUsername]=useState('')
+
+  return (
+    <div className='App'>
+   <div className='addUser'>
+    <input type='text' placeholder='Name...' onChange={e=>{setName(e.target.value)}}/>
+    <input type='text' placeholder='Username...' onChange={e=>{setUsername(e.target.value)}}/>
+    <button onClick={()=>dispatch(addUser({id:userList[userList.length -1].id + 1,
+      name,username}))}>Add User</button>
+   </div>
+   <div className='displayUsers'>
+    {userList.map((user)=>(
+      <div className='user' key={user.id}>
+        <h3>{`User:${user.name}`}</h3>
+        <h3>{`Username:${user.username}`}</h3>
+        <input type='text' placeholder='New Username...' onChange={e=>{setNewUsername(e.target.value)
+        }}/>
+        <button onClick={()=>{dispatch(updateUsername({id:user.id,username:newUsername}))}}>Update Username</button>
+        <button onClick={()=>{dispatch(deleteUser({id:user.id}))}}>Delete User</button>
+      </div>
+    ))}
+   </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ rdx-crud/src/FakeData.js
@@ -0,0 +1,32 @@
+export const UsersData=[
+    {
+        id:1,
+        name:"abed rivaz",
+        username:"xavi"
+    },
+    {
+        id:2,
+        name:"mojtaba parvar",
+        username:"van persi",
+    },
+    {
+        id:3,
+        name:"vahid parkale",
+        username:"cris"
+    },
+    {
+        id:4,
+        name:"mohamad sokhon",
+        username:"mamali"
+    },
+    {
+        id:5,
+        name:"hadi moslemi",
+        username:"capi"
+    },
+    {
+        id:6,
+        name:"yahya parvar",
+        username:"masheii"
+    },
+]
